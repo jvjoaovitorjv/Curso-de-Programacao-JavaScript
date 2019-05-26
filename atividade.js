f909a3a9e9d87801c62d14b6e9dca52a12075f6a
jvjoaovitorjv <jvvitor200150@gmail.com>
2019-05-26 20:51:53 +0000

    atividade 04

---
+++ atividade.js
@@ -0,0 +1,16 @@
+var idade = 18
+if (idade > 0 && idade < 12){
+    console.log("você é criança!!")
+}
+
+if (idade >=12 && idade < 19){
+    console.log("você é adolescente!!")
+}
+
+if (idade >=19 && idade <= 40){
+    console.log("você é adulto!!")
+}
+
+if (idade > 40){
+    console.log("você é velho!!")
+}
