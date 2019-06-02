9ebea1fe52b3ed2926f750cf7f0e2e5ce32ee4d3
jvjoaovitorjv <jvvitor200150@gmail.com>
2019-06-02 17:55:39 -0300

    atividade 04 corrigido

--- atividade.js
+++ atividade.js
@@ -3,14 +3,14 @@ if (idade > 0 && idade < 12){
     console.log("você é criança!!")
 }
 
-if (idade >=12 && idade < 19){
+else if (idade >=12 && idade < 19){
     console.log("você é adolescente!!")
 }
 
-if (idade >=19 && idade <= 40){
+else if (idade >=19 && idade <= 40){
     console.log("você é adulto!!")
 }
 
-if (idade > 40){
+else if (idade > 40){
     console.log("você é velho!!")
 }
